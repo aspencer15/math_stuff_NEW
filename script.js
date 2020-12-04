2951099c9f36a7ccfbfb70cdb1bb73b80a8a88e8
aspencer15 <amycspencer@gmail.com>
2020-12-03 22:11:46 -0500

    first commit of changes

---
+++ script.js
@@ -0,0 +1,15 @@
+// function multiply() {
+//     let firstNumber = Number(document.getElementById=('numberOne').value);
+//     let secondNumber = Number(document.getElementById=('numberTwo').value);
+//     let answer = firstNumber*secondNumber;
+//     // console.log(answer)
+//     document.getElementById('answer').value = answer;
+// }
+
+function mul(){
+  
+    let one= Number(document.getElementById('nume').value);
+    let two= Number(document.getElementById('numo').value);
+    let three = one*two;
+    document.getElementById('numree').value = three;
+  }

591b7310bd0c2af505aec81a891461d2b19299a0
Savitha <savidak@gmail.com>
2020-12-04 15:15:17 -0500

    mathdiv

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function division() {
+  
+    let one= Number(document.getElementById('numOne').value);
+    let two= Number(document.getElementById('numTwo').value);
+    let three = one/two;
+    document.getElementById('numThree').value = three;
+ 
+}

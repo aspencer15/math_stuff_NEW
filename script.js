8402ff14d4caab284ad7d2ec97dda53b0297123e
aspencer15 <amycspencer@gmail.com>
2020-12-07 14:53:16 -0500

    third commit

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 function multiply() {
-    let one = Number(document.getElementById=('numberOne').value);
-    let two = Number(document.getElementById=('numberTwo').value);
+    let one = Number(document.getElementById('numberOne').value);
+    let two = Number(document.getElementById('numberTwo').value);
     let three = one*two;
     console.log(three)
     document.getElementById('answer').value = three;
 }
 
-// function mul(){
-  
-//     let one= Number(document.getElementById('nume').value);
-//     let two= Number(document.getElementById('numo').value);
-//     let three = one*two;
-//     document.getElementById('numree').value = three;
-//   }
+
